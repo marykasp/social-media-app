971213ae06de9c3739b00b7af0e6d0de5b8dae92
marykasp <kasparian.mary@gmail.com>
2023-09-13 11:04:22 -0500

    chore: setup initial form for login and register
    Use formik and yup for handling form and form validations. Setup the
    validations for registering a user and logggin in.

--- client/src/theme.js
+++ client/src/theme.js
@@ -60,7 +60,7 @@ export const themeSettings = (mode) => {
             primary: {
               dark: colorTokens.primary[700],
               main: colorTokens.primary[500],
-              light: colorTokens.primary[50],
+              light: colorTokens.primary[200],
             },
             neutral: {
               dark: colorTokens.grey[700],
