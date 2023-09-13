27b79ba94a802bfbbaba9d5bdc77b387a9aff22a
marykasp <kasparian.mary@gmail.com>
2023-09-13 09:37:05 -0500

    chore: import components, icons, and functions required for navbar
    Setup toggled state, get state from reducer and useMediaQuery hook to
    determine screen size and not have to use mediq queries in css.

--- client/src/scenes/navbar/index.jsx
+++ client/src/scenes/navbar/index.jsx
@@ -1,4 +1,40 @@
+import { useState } from "react";
+import {
+  Box,
+  IconBUtton,
+  InputBase,
+  Typography,
+  Select,
+  MenuItem,
+  FormControl,
+  useTheme,
+  useMediaQuery,
+} from "@mui/material";
+import {
+  Search,
+  Message,
+  DarkMode,
+  LightMode,
+  Notifications,
+  Help,
+  Menu,
+  Close,
+} from "@mui/icons-material";
+import { useDispatch, useSelector } from "react-redux";
+import { setMode, setLogout } from "state";
+import { useNavigate } from "react-router-dom";
+import FlexBetween from "components/FlexBetween";
+
 const NavBar = () => {
+  const [isMobileMenuToggled, setIsMobileMenuToggled] = useState(false);
+  const dispatch = useDispatch(); // dispatch actions from reducers
+  const navigate = useNavigate();
+  const user = useSelector((state) => state.user);
+  // determines if screen width is below a min-width (React hook from mui)
+  const isNonMobileScreens = useMediaQuery("(min-width: 10000px)");
+
+  const theme = useTheme();
+
   return <div>NavBar</div>;
 };
 
